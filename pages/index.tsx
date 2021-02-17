cad61c4431577a0b390bee174a509512ae469dcb
Son Chau <sonchau24@yahoo.com>
2021-02-17 15:28:25 +1100

    [#85] add style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,31 @@
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
 import { initializeApollo } from '../lib/client';
-import { useTasksQuery, TasksQuery, TasksDocument } from '../generated/graphql-frontend';
+import {
+  useTasksQuery,
+  TasksQuery,
+  TasksDocument,
+} from '../generated/graphql-frontend';
+import TaskList from '../components/TaskList';
 
 export default function Home() {
   const result = useTasksQuery();
   const tasks = result.data?.tasks;
 
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Tasks</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      {tasks &&
-        tasks.length > 0 &&
-        tasks.map((task) => {
-          return (
-            <div key={task.id}>
-              {task.title} ({task.status})
-            </div>
-          );
-        })}
+      {result.loading ? (
+        <p>Loading tasks...</p>
+      ) : result.error ? (
+        <p>An error occurred.</p>
+      ) : tasks && tasks.length > 0 ? (
+        <TaskList tasks={tasks} />
+      ) : (
+        <p className="no-tasks-message">You've got no tasks.</p>
+      )}
     </div>
   );
 }
