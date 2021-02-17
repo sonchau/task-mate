3b2a7628a59b542a0839f121395c88886bf9da32
Son Chau <sonchau24@yahoo.com>
2021-02-17 14:50:31 +1100

    [#83] set up apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css';
 import { AppProps } from 'next/app';
+import { useApollo } from '../lib/client';
+import { ApolloProvider } from '@apollo/client';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const apolloClient = useApollo(pageProps.initialApolloState);
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
 export default MyApp;
